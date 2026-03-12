696ac94ec34faae8d301f35d0b43f96c42b519cf
rober122pst <jrobertinhor.9@gmail.com>
2026-03-12 10:48:57 -0300

    fazendo a sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import { LogoSport } from './components/logo-sport';
+
 function App() {
     return (
-        <div className="">
-            <aside></aside>
-            <main></main>
+        <div className="grid h-screen grid-flow-col grid-cols-[32.5%_1fr] gap-5 overflow-hidden px-5">
+            <aside className="bg-shape-body my-5 rounded-xl">
+                <div>
+                    <LogoSport size={128} />
+                </div>
+            </aside>
+            <main className="bg-shape-body my-5 overflow-auto rounded-xl">
+                <div></div>
+            </main>
         </div>
     );
 }
